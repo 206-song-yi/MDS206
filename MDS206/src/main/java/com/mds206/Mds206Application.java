325487b7df1e8d9a3e178b3e8bf17b6b12d44c91
geungwan <kevinyook07@gmail.com>
2019-08-22 11:35:16 +0900

    팀프로젝트 생성

---
+++ MDS206/src/main/java/com/mds206/Mds206Application.java
@@ -0,0 +1,13 @@
+package com.mds206;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Mds206Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Mds206Application.class, args);
+	}
+
+}
---
+++ MDS206/src/main/java/com/mds206/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.mds206;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(Mds206Application.class);
+	}
+
+}
